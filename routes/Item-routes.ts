8fefeba4bf347f6b935a2177f14072eabc42037c
UvinduV <uvindusanketh470@gmail.com>
2025-01-29 00:58:17 +0530

    create get item

--- routes/Item-routes.ts
+++ routes/Item-routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import {Item} from "../model/Item";
-import {ItemAdd} from "../database/prisma-data-store";
+import {getAllItem, ItemAdd} from "../database/prisma-data-store";
 
 const router = express.Router();
 
@@ -12,6 +12,15 @@ router.post('/add',async (req,res,next)=>{
     }catch(err){
         console.log("error adding item", err);
         res.status(400).send("error adding item");
+    }
+})
+
+router.get('/view',async (req,res,next)=>{
+    try{
+        const items=  await getAllItem();
+        res.json(items);
+    }catch(err){
+        console.log("error getting items", err);
     }
 })
 
